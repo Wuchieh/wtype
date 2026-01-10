75394d610c1d82afb175a4ee5743646f70c02b3f
wuchieh <wuchieh@wuchieh.com>
2026-01-10 18:30:28 +0800

    test: refactor TestSafeCache

--- safe_cache_test.go
+++ safe_cache_test.go
@@ -10,17 +10,20 @@ import (
 
 func TestSafeCache(t *testing.T) {
 	s := wtype.NewSafeCache(time.Second, 0)
-
+	s2 := 0
 	wg := sync.WaitGroup{}
 	wg.Add(1000)
 	for i := 0; i < 1000; i++ {
 		go func(index int) {
 			defer wg.Done()
+			s2++
 			s.Use(func(data int) int {
-				t.Logf("data: %d, index: %d", data, index)
 				return data + 1
 			})
 		}(i)
 	}
 	wg.Wait()
+	if s.Get() != 1000 {
+		t.Errorf("Get %d, want 1000", s.Get())
+	}
 }
